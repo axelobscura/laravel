88bde43a104871b5684df3f45083bf454ade3bda
Axel <axosar@gmail.com>
2020-06-15 11:31:15 -0500

    Resources and Controllers

--- routes/web.php
+++ routes/web.php
@@ -24,11 +24,15 @@ Route::get('/contact', function () {
     return 'Contact page...';
 });
 
-Route::get('/post/{id}/{name}', function($id, $name){
-    return "This is the post ".$id." and the name is ".$name;
-});
+//Route::get('/post/{id}/{name}', function($id, $name){
+//    return "This is the post ".$id." and the name is ".$name;
+//});
 
 Route::get('/admin/posts/example', array('as'=>'admin.home', function(){
     $url = route('admin.home');
     return "This is url ".$url;
 }));
+
+//Route::get('/post/{nombre}', 'PostsController@index');
+
+Route::resource('posts', 'PostsController');
