342f13b764a8316dc15dea1d2b52bf6a34ae024c
Axel <axosar@gmail.com>
2020-06-21 12:44:35 -0500

    Título

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    AWEBO
                 </div>
 
                 <div class="links">
